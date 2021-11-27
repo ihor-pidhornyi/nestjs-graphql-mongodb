c7898504d526228e9a87b670f22074ae8f454986
ihorpidhornyi <ihorp@rabota.ua>
2021-11-27 13:39:16 +0200

    feat(student): implemented student queries and mutation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    LessonModule,
     TypeOrmModule.forRoot({
       type: 'mongodb',
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
+    LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
---
+++ src/student/student.resolver.ts
@@ -0,0 +1,26 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+import { StudentService } from './student.service';
+
+@Resolver((of) => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
+
+  @Query((returns) => StudentType)
+  student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Mutation((returns) => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
